67663c8c958015858611f5da6faac6fae68411d3
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-02 17:48:50 -0500

    AComit

--- server/lib/requireRouts.js
+++ server/lib/requireRouts.js
@@ -5,8 +5,7 @@ module.exports = function(_s){
     _s.oReq.app.use(_s.oReq.bodyParser.urlencoded({ extended: true }));
     _s.oReq.app.use(_s.oReq.express.static(_s.sClientDirname + '/css'));
     _s.oReq.app.use(_s.oReq.express.static(_s.sClientDirname + '/js'));
-    _s.oReq.app.use(_s.oReq.express.static(_s.sClientDirname + '/api'));
-    console.log(_s.sClientDirname);
+    _s.oReq.app.use(_s.oReq.express.static((_s.sClientDirname + '/api')));
     //_s.oReq.app.use(_s.oReq.express.static(_s.sClientDirname));
 
 
