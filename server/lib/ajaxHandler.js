738e4637fdc329d3ebc40836caf0214140649691
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-08 22:12:29 -0500

    AComit

--- server/lib/ajaxHandler.js
+++ server/lib/ajaxHandler.js
@@ -1,6 +1,6 @@
 // export the class
 module.exports = function(req, res, _s) {
-    console.log(arguments);
+    console.log(_s);
     var _this = this;
     var _ = _s.oReq.lodash;
     this.body = req.body;
