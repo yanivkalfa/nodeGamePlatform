46da364389019bdff8d6706d6ab9a9dcdfdedb26
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-09 17:53:14 -0500

    AComit

--- client/api/modules/main/ngp.node-game-platform.js
+++ client/api/modules/main/ngp.node-game-platform.js
@@ -1,4 +1,15 @@
 /**
  * Created by Yaniv-Kalfa on 1/2/15.
  */
-angular.module(ngp.const.app.name, ['ui.router']);
+angular.module(ngp.const.app.name, ['ui.router']).run(['$rootScope', '$state', '$stateParams', 'authorization', 'principal', runFactory]);
+
+function runFactory($rootScope, $state, $stateParams, authorization, principal) {
+    $rootScope.$on('$stateChangeStart', function(event, toState, toStateParams) {
+        // track the state the user wants to go to; authorization service needs this
+        $rootScope.toState = toState;
+        $rootScope.toStateParams = toStateParams;
+        // if the principal is resolved, do an authorization check immediately. otherwise,
+        // it'll be done when the state it resolved.
+        if (principal.isIdentityResolved()) authorization.authorize();
+    });
+}
