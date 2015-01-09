f90588d102ec63ecd3c3752f5ece2c62c6e5aa12
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-09 17:13:46 -0500

    AComit

--- server/core/models.js
+++ server/core/models.js
@@ -1,11 +1,11 @@
 module.exports = function(_s, connection){
-    var models = require('../settings/models'),modelsSchema, modelName, schema, model = [];
+    var models = require('../settings/models'),modelsSchema, modelName, schema, allModels = [];
 
     models.forEach(function(model){
         schema = new _s.oReq.mongoose.Schema(model.schema);
         global[model.name] = connection.model(model.name, schema);
-        model.push(global[model.name]);
+        allModels.push(global[model.name]);
     });
 
-    return this;
+    return allModels;
 };
