1b468e21ff87faa3b3696d0f683648e91ddcdb80
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-10 01:15:52 -0500

    AComit

--- client/api/modules/main/components/services/ngp.notifyService.js
+++ client/api/modules/main/components/services/ngp.notifyService.js
@@ -27,6 +27,7 @@ function notifyFactory($rootScope) {
             console.log(this, 'happeneing');
             this.msg = '';
             this.show = false;
+
             $rootScope.$apply()
         },
 
@@ -47,7 +48,7 @@ function notifyFactory($rootScope) {
             this.msg = msg;
             this.show = true;
             if(this.options.timeout){
-                setTimeout(_self.reset,_self.options.timeout);
+                setTimeout(_.bind(_self.reset, $rootScope.notify),_self.options.timeout);
             }
         }
     };
