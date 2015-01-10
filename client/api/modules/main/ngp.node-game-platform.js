86d623aec8c32d89ffcff48e6613687f01658642
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-10 00:17:13 -0500

    AComit

--- client/api/modules/main/ngp.node-game-platform.js
+++ client/api/modules/main/ngp.node-game-platform.js
@@ -6,6 +6,11 @@ angular.module(ngp.const.app.name, ['ui.router', 'ngCookies']).run(['$rootScope'
 function runFactory($rootScope, $state, $stateParams, authorization, User) {
     $rootScope.$on('$stateChangeStart', function(event, toState, toStateParams) {
 
+        $rootScope.notify = {
+            show : true,
+            class : 'success',
+            msg : 'yaniv !'
+        };
         $rootScope.toState = toState;
         $rootScope.toStateParams = toStateParams;
 
