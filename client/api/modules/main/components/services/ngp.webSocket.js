5705a50cf531ee8fe955d54a9a4a8526d037c8eb
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-10 22:38:50 -0500

    AComit

--- client/api/modules/main/components/services/ngp.webSocket.js
+++ client/api/modules/main/components/services/ngp.webSocket.js
@@ -6,32 +6,36 @@ angular.module(ngp.const.app.name)
     .service('WebSocket', [
         '$rootScope',
         'User',
-        WebSocket
+        webSocket
     ]);
 
-function WebSocket($rootScope, User) {
-    this.Primus = false;
-    this.user = User;
-    this.init();
+function webSocket($rootScope, User) {
 
-}
 
-WebSocket.prototype =  {
-
-    init : function(){
-        console.log(this);
-
-        if(!this.user.isAuthenticated) return false;
-
-        var token = this.user.get().token;
-        this.Primus = Primus.connect('ws://' + ngp.const.app.domain + '/?token=' + token);
-
-        this.Primus.on('msg', function(msg){
-            this[msg.method](msg.data);
-
-        });
-
-        return true;
+    function WebSocket(){
+        this.Primus = false;
+        this.init();
     }
 
-};
+    WebSocket.prototype =  {
+
+        init : function(){
+
+            if(!User.isAuthenticated) return false;
+
+            var token = User.get().token;
+            this.Primus = Primus.connect('ws://' + ngp.const.app.domain + '/?token=' + token);
+
+            this.Primus.on('msg', function(msg){
+                this[msg.method](msg.data);
+
+            });
+
+            return true;
+        }
+
+    };
+
+
+    return new WebSocket();
+}
