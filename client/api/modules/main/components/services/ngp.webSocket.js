c79c1f0b431a8add85de086dfd124519a35eb677
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-10 23:51:42 -0500

    AComit

--- client/api/modules/main/components/services/ngp.webSocket.js
+++ client/api/modules/main/components/services/ngp.webSocket.js
@@ -21,7 +21,7 @@ function webSocket($rootScope, User) {
             var token = User.get().token;
             this.Primus = Primus.connect('ws://' + ngp.const.app.domain + '/?token=' + token);
             this.Primus.on('data', function(msg){
-                console.log(msg);
+                console.log(this);
                 this[msg.method](msg.data);
 
             });
