22271656c42cc181f68018fdbf7d9484fdd310c5
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-11 20:42:53 -0500

    AComit

--- client/api/modules/main/ngp.node-game-platform.js
+++ client/api/modules/main/ngp.node-game-platform.js
@@ -21,10 +21,7 @@ function runFactory(
     CronJobs.add(cfn);
 
     var cfn = {fn: 'authenticateUser', f : function(){console.log('aaaaa');},  args : '', execEvery : 1000, lastExec : false, ref : false};
-    CronJobs.add(cfn);
-
-    var cfn = {fn: 'test', f : function(){console.log('bbbb');},  args : '', execEvery : 1000, lastExec : false, ref : false};
-    CronJobs.add(cfn);
+    CronJobs.update(cfn);
 
     $rootScope.$on('$stateChangeStart', function(event, toState, toStateParams) {
 
