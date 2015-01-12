ac95931f51b931c966086636eacfe95581e20c1d
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-11 21:59:56 -0500

    AComit

--- server/lib/modules/ajaxHandler.js
+++ server/lib/modules/ajaxHandler.js
@@ -28,7 +28,7 @@ module.exports = function(_s, req, res) {
     };
 
     this.authenticateUser = function(userDetails){
-        //return this.logout();
+        return this.logout();
         _this._setResp('User is not logged in', false);
         if(req.session.user && userDetails && req.session.user.token == userDetails.token){
             _this._setResp(true, true);
