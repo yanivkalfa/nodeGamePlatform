d0a938cc49f50b52ff9084a4cfbdf888c2db1a0d
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-12 11:10:16 -0500

    AComit

--- server/lib/modules/User.js
+++ server/lib/modules/User.js
@@ -50,6 +50,7 @@ module.exports = function(_s){
             });*/
 
             _s.primus.forEach(function (spark, next) {
+                console.log(next);
                 console.log('spark.userId', spark.userId);
                 if(spark.userId == req.session.user.id){
                     spark.end();
