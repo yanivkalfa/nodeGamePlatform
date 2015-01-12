8f4bfbf3814fbd3f412d1207dbb13ea2ea552098
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-11 23:59:37 -0500

    AComit

--- server/lib/modules/index.js
+++ server/lib/modules/index.js
@@ -3,7 +3,7 @@ module.exports = function(_s){
     rf.User = require('./User.js')(_s);
     rf.uf = require('./utilFunc.js')(_s);
     rf.ajaxHandler = require('./ajaxHandler.js');
-    rf.WebSocket = require('./websocket.js');
+    rf.WebSocket = require('./webscoket.js');
 
     return rf;
 };
