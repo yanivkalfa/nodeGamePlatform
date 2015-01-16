a93f7472fa7d115e95216492f330c4aa8f8ab261
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-15 23:19:29 -0500

    AComit

--- client/api/modules/main/components/controllers/ngp.adminController.js
+++ client/api/modules/main/components/controllers/ngp.adminController.js
@@ -48,6 +48,11 @@ function adminController(
 
         };
 
+        $scope.tabs = [
+            { title:'Dynamic Title 1', content:'Dynamic content 1' },
+            { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+        ];
+
     }
 
     AdminController.prototype.logout = function(){
