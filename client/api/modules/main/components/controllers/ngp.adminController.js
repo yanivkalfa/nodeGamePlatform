12f3200b3a11bd2be9487a09a416376d34d97799
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-19 14:28:48 -0500

    AComit

--- client/api/modules/main/components/controllers/ngp.adminController.js
+++ client/api/modules/main/components/controllers/ngp.adminController.js
@@ -50,7 +50,7 @@ function adminController(
 
         $scope.tabs = [
             { title:'Dynamic Title 1', content:'Dynamic content 1' },
-            { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+            { title:'Dynamic Title 2', content:'Dynamic content 2' }
         ];
 
     }
