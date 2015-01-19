bcc5477afec5be175b06e270d88cdd2804fa97a3
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-19 14:16:53 -0500

    AComit

---
+++ client/api/modules/main/components/directives/ngp.tabs.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Yaniv-Kalfa on 1/19/15.
+ */
+/**
+ * Created by Yaniv-Kalfa on 1/2/15.
+ */
+angular.module(ngp.const.app.name)
+    .directive('ngpTabContent', [ngpTabContent]);
+
+function ngpTabContent() {
+    return {
+        templateUrl: ngp.const.app.url + '/tpl/directives/tabContent.html'
+    };
+}
