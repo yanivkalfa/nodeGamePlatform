b87ddcf5be374fd101ee5818c2c83afa47f2353b
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-19 14:38:56 -0500

    AComit

--- client/api/modules/main/components/directives/ngp.tabsContent.js
+++ client/api/modules/main/components/directives/ngp.tabsContent.js
@@ -2,12 +2,10 @@
  * Created by Yaniv-Kalfa on 1/19/15.
  */
 angular.module(ngp.const.app.name)
-    .directive('ngpTabContent', [ngpTabContent]);
-
-function ngpTabContent() {
+    .directive('ngpTabContent', function() {
 
     console.log(ngp.const.app.url + '/tpl/directives/tabContent.html');
     return {
         templateUrl: ngp.const.app.url + '/tpl/directives/tabContent.html'
     };
-}
+});
