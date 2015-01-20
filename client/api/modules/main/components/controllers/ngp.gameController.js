d577a80944ebef520cb4e4db944e928463170d36
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-20 15:03:58 -0500

    AComit

--- client/api/modules/main/components/controllers/ngp.gameController.js
+++ client/api/modules/main/components/controllers/ngp.gameController.js
@@ -3,32 +3,13 @@
  */
 angular.module(ngp.const.app.name)
     .controller('gameController', [
-        '$rootScope',
-        '$scope',
-        '$state',
-        '$location',
-        '$cookieStore',
-        'Api',
-        'User',
-        'Notify',
-        'WebSocket',
-        'Latency',
-        'InitChat',
         gameController
     ]);
 
-function gameController(
-    $rootScope,
-    $scope,
-    $state,
-    $location,
-    $cookieStore,
-    Api,
-    User,
-    Notify,
-    WebSocket,
-    Latency,
-    InitChat
-    ) {
+function gameController() {
+
+    function GameController(){ }
+
+    return new GameController();
 
 }
