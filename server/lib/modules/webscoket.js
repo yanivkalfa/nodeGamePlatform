87334e5cea7483b714f0d0cb2d8a790d54896802
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-20 21:19:20 -0500

    AComit

--- server/lib/modules/webscoket.js
+++ server/lib/modules/webscoket.js
@@ -1,6 +1,5 @@
 // export the class
 module.exports = function() {
-    /*
     function WebSocket(_s, Primus, spark){
         this._s = _s;
         this.Primus = Primus;
@@ -20,17 +19,8 @@ module.exports = function() {
             });
 
         }
-    };*/
-
-    return {
-        init : function(_s, Primus, spark){
-            var self = this;
-
-            spark.on('data', function (msg) {
-                console.log(msg);
-                self[msg.m](spark, msg.d);
-            });
-        }
     };
+
+    return WebSocket;
 };
 
