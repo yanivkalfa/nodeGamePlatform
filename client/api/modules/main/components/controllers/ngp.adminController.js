5592e120f52ed5eb9862faa82d3cf6c2f2f92c2f
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-20 23:59:09 -0500

    AComit

--- client/api/modules/main/components/controllers/ngp.adminController.js
+++ client/api/modules/main/components/controllers/ngp.adminController.js
@@ -11,6 +11,7 @@ angular.module(ngp.const.app.name)
         'Api',
         'User',
         'Notify',
+        'WebSocket',
         adminController
     ]);
 
@@ -22,15 +23,14 @@ function adminController(
     $cookieStore,
     Api,
     User,
-    Notify
+    Notify,
+    WebSocket
     ) {
 
     function AdminController(){
         var self = this;
         this.api = Api.createNewApi(ngp.const.app.ajaxUrl);
         this.User = User.get();
-
-        console.log(this.User);
     }
 
     AdminController.prototype.logout = function(){
