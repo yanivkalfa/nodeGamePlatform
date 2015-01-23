baf16699f10d42aa33c76c827a00e3ebc8875cac
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-22 21:48:39 -0500

    AComit a

--- server/lib/modules/regValidation.js
+++ server/lib/modules/regValidation.js
@@ -1,8 +1,14 @@
 module.exports = function(_s){
 
+    var validation = _s.oModules.Validation
+        , _ = _s.oReq.lodash
+        ;
+
     function RegValidation (details){
-        _s.oModules.Validation.call(this,details);
+        validation.call(this,details);
     }
+
+    _.assign(RegValidation.prototype, validation.prototype);
 
     return RegValidation;
 };
