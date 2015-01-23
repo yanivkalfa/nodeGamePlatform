b084b903ec19e6336483d69ba43379973f6b0db9
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-23 06:24:10 -0500

    AComit a

--- server/lib/modules/routRoom.js
+++ server/lib/modules/routRoom.js
@@ -86,6 +86,8 @@ module.exports = function(_s, _rf){
 
                         var endTime = Date.now();
 
+                        console.log(endTime);
+                        console.log(starTime);
                         console.log(endTime-starTime);
                     };
 
