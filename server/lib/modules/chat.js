ae1b0947546575e27dc3b5f04354f4d8b7096110
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-22 23:03:25 -0500

    AComit a

--- server/lib/modules/chat.js
+++ server/lib/modules/chat.js
@@ -1,16 +1,19 @@
 module.exports = function(_rf){
 
     var router = _rf.Router;
+    console.log(router);
 
     function Chat (Primus, spark){
         router.call(this,Primus, spark);
 
-        this.RouterMsg = new _rf.RouterMsg(Primus, spark);
-        this.RouterRoom = new _rf.RouterRoom(Primus, spark);
+        //this.RouterMsg = new _rf.RouterMsg(Primus, spark);
+        //this.RouterRoom = new _rf.RouterRoom(Primus, spark);
     }
 
     Chat.prototype = Object.create(router.prototype);
     Chat.prototype.constructor = Chat;
+
+    /*
 
     Chat.prototype.msg = function(msg){
         this.RouterMsg.rout(msg);
@@ -18,7 +21,7 @@ module.exports = function(_rf){
 
     Chat.prototype.roomDo = function(msg){
         this.RouterRoom.rout(msg);
-    };
+    };*/
 
 
     return Chat;
