0ed2ae596ddb378e7415e61ec4bb6e67b37ba700
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-24 15:33:44 -0500

    AComit a

--- server/app.js
+++ server/app.js
@@ -104,7 +104,7 @@ _s.primus.on('connection', function (spark) {
                                 "d" : {
                                     "m" : 'getRooms',
                                     "d" : {
-                                        rooms : user.rooms
+                                        rooms : user.rooms || undefined
                                     }
                                 }
                             };
