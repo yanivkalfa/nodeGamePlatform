21cde52ba641b60d2b5b92856a412a2634671490
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-24 15:31:09 -0500

    AComit a

--- server/app.js
+++ server/app.js
@@ -64,14 +64,14 @@ _s.primus.on('connection', function (spark) {
                 }
                 else
                 {
-                    console.log('decoded', decoded);
+
                     // Attaching user to spark - for logout and maybe future needs
                     spark.user = user;
 
                     // Update user's spark id in database - in-case its needed
                     var upSkSuccess = function (user){
 
-                        console.log('Not suppose to get here', spark);
+                        console.log('User', user);
                         console.log(user.rooms);
                         // Joining terminal, lobby  and user rooms
                         var userRoom = 'u_' + decoded.userId;
