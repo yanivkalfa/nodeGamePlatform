3442ca6d772b98d40e0f670cdbe89313b9307abf
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-25 16:12:52 -0500

    AComit a

--- server/lib/requiredRouts.js
+++ server/lib/requiredRouts.js
@@ -17,6 +17,7 @@ module.exports = function(_s){
     _s.oReq.app.get('/contents/:content', function (req, res) {
         if(_s.oConfig.routs.loginRequired(req.params.content)){
             _s.oModules.Authorization.init(req.session.user, _s.oConfig.routs.getRout(req.params.content));
+            console.log(req.session.user);
             if(_s.oModules.Authorization.isAuthenticated()){
                 res.render(_s.sServerDirname + '/tpl/contents/' + req.params.content + '.jade');
             }else{
