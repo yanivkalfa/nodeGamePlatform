91497abbde4ed1421f0655375a0bcd3f5728f4e5
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-25 15:03:29 -0500

    AComit a

--- client/api/modules/main/components/services/ngp.authorization.js
+++ client/api/modules/main/components/services/ngp.authorization.js
@@ -172,6 +172,7 @@ function Authorization(
         },
 
         isSet: function() {
+            console.log('this._user', this._user);
             return this._user && angular.isDefined(this._user.token)
         },
         isAuthenticated: function() {
