4eb9d15f01f5bcb0c2039aafb9038f1d7b9a3b74
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-28 06:59:18 -0500

    AComit a

--- server/lib/requiredWebSockets.js
+++ server/lib/requiredWebSockets.js
@@ -42,7 +42,7 @@ module.exports = function(_s){
             "method" : 'login',
             "status" : 0,
             "success" : false,
-            "data" : "aaa"
+            "data" : credential
         }
         ;
 
