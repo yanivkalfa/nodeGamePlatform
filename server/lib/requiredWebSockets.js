4cd1b8c09462fe1e703afac85138ad3aff3ba4b5
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-28 07:44:31 -0500

    AComit a

--- server/lib/requiredWebSockets.js
+++ server/lib/requiredWebSockets.js
@@ -141,9 +141,11 @@ module.exports = function(_s){
                     }
 
                 }).catch(function(err){
+                    console.log(err);
                     if(err) spark.end({"method" : "disconnect", msg : "Could not authenticate user b."} );
                 });
             }else{
+                console.log(err);
                 spark.end({"method" : "disconnect", msg : "Could not authenticate user c."} );
             }
         });
