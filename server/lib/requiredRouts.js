00420621a4f74c6c491271297c0d3ca451a0a609
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-27 21:49:12 -0500

    AComit a

--- server/lib/requiredRouts.js
+++ server/lib/requiredRouts.js
@@ -1,20 +1,5 @@
 
 module.exports = function(_s){
-    var _ = _s.oReq.lodash,
-        sessCon = _s.oConfig.session.connection,
-        sessSecret = _s.oConfig.session.secret,
-        sessMaxAge = _s.oConfig.session.maxAge;
-
-    _s.oReq.app.use(_s.oReq.session({
-        store: new _s.oReq.RedisStore({
-            port : _s.oConfig.connections[sessCon].port,
-            host : _s.oConfig.connections[sessCon].host
-        }),
-        secret: sessSecret,
-        saveUninitialized: true,
-        resave: true,
-        cookie: { maxAge: sessMaxAge }
-    }));
 
     _s.oReq.app.use(_s.oReq.bodyParser.json());
     _s.oReq.app.use(_s.oReq.bodyParser.urlencoded({ extended: true }));
