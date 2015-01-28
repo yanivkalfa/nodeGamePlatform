25537a8dd765fa2c6f57ccb3513d8aa799e4163e
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-28 16:33:15 -0500

    AComit a

--- server/app.js
+++ server/app.js
@@ -6,12 +6,12 @@ _s.oConfig = require('./settings/config'); // require config files.
 global.oCore = require('./core')(_s); // require core files.
 _s.oModules = require('./lib/modules')(_s); // require utility functions
 
-var Servers = new _s.oModules.Servers();
+var servers = new _s.oModules.Servers();
 var sName =  process.argv[2] || 'testSlave01';
 
 if(sName.toLowerCase() == 'servers'){
     var filter = function(item, i){ return i !== 0 && i !== 1 && i !== 2; };
-    Servers.analys(process.argv.filter(filter));
+    servers.analys(process.argv.filter(filter));
     process.exit();
 }else{
 
@@ -31,6 +31,6 @@ if(sName.toLowerCase() == 'servers'){
         });
     };
 
-    Servers.fetchByName(sName).then(startServer).catch(console.log)
+    servers.fetchByName(sName).then(startServer).catch(console.log)
 
 }
