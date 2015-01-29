0410a3b8ff352a8c290f52945b644220bfbdeed2
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-01-29 00:40:54 -0500

    AComit a

--- server/lib/requiredWebSockets.js
+++ server/lib/requiredWebSockets.js
@@ -93,7 +93,7 @@ module.exports = function(_s){
                         });
 
                         _s.primus.metroplex.servers(function (err, servers) {
-                            console.log('registered servers:', servers);
+                            //console.log('registered servers:', servers);
                         });
 
                     }
