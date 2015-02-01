52f8f3880c332126a9ec5c412fefc7180bfc2d37
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-02-01 16:54:11 -0500

    AComit a

--- server/lib/modules/gamesApi.js
+++ server/lib/modules/gamesApi.js
@@ -6,36 +6,51 @@ module.exports = function(_s){
 
     ServersClass.prototype =  {
 
+        filter : function(games){
+            var self = this;
+            if(_.isEmpty(games)) return false;
+            games = _.isArray(games) ? games : [games];
+            _(games).forEach(function(game, i){
+                games[i] = _.pick(games, self.visibleField);
+            });
+
+            return games;
+        },
+
 
         fetch : function(query){
+            var self = this;
             return new _s.oReq.Promise(function(resolve, reject) {
                 Games.find(query).exec(function (err, succ) {
                     if(err) return reject(err);
-                    return resolve(succ);
+                    return resolve(self.filter(succ));
                 });
             });
         },
 
         fetchByName : function(name){
+            var self = this;
             return new _s.oReq.Promise(function(resolve, reject) {
                 Games.findOne({name : name}).exec(function (err, succ) {
                     if(err) return reject(err);
-                    return resolve(succ);
+                    return resolve(self.filter(succ));
                 });
             });
         },
 
         add : function(game){
+            var self = this;
             return new _s.oReq.Promise(function(resolve, reject) {
                 if('object' !== typeof game|| _.isEmpty(game)) return reject('Please supply correct game details');
 
-                var success = function(game){ return resolve(game); };
+                var success = function(game){ return resolve(self.filter(game)); };
                 var fail = function(err){ return reject(err); };
                 Games.create(game).then(success,fail);
             });
         },
 
         remove : function(game){
+            var self = this;
             return new _s.oReq.Promise(function(resolve, reject) {
                 if(_.isEmpty(game)) return reject('We could not remove this game');
                 Games.remove({ name: game }, function (err) {
