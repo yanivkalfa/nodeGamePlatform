d8e115f2795225473ab3651345f2085696e4f590
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-02-01 22:01:51 -0500

    AComit a

--- client/modules/main/components/factories/ngp.routQueue.js
+++ client/modules/main/components/factories/ngp.routQueue.js
@@ -4,14 +4,11 @@
 angular.module(ngp.const.app.name)
     .factory('RoutQueue', [
         '$rootScope',
-        '$scope',
-        'Authorization',
-        'Chat',
         'Router',
         RoutQueue
     ]);
 
-function RoutQueue($rootScope,$scope,Authorization, Chat, Router) {
+function RoutQueue($rootScope,Router) {
 
     function RoutQueueFactory(){
         Router.apply(this, arguments);
@@ -21,37 +18,11 @@ function RoutQueue($rootScope,$scope,Authorization, Chat, Router) {
     RoutQueueFactory.prototype.constructor = RoutQueueFactory;
 
 
-    RoutQueueFactory.prototype.Ready = function(room){
+    RoutQueueFactory.prototype.ready = function(room){
 
-        var user = Authorization.getUser();
-        if(!_.isArray(room.users) && user.id == room.users.id) {
-            Chat.joinRoom(Chat.createRoom(room));
-        }
-        else
-        {
-            if(!_.isArray(room.users)) {
-                Chat.addMember(room.users, room);
-            }
-            else{
-                if(Chat.indexOf(room) == -1) {
-                    Chat.joinRoom(Chat.createRoom(room));
-                }else{
-                    _(room.users).forEach(function(user){
-                        Chat.addMember(user, room);
-                    })
-                }
-
-            }
-        }
-
-        $rootScope.$apply();
     };
 
     RoutQueueFactory.prototype.leave = function(room){
-        var user = Authorization.getUser();
-        if(user.id == room.users.id) Chat.leaveRoom(room);
-        else Chat.removeMember(room.users, room);
-        $rootScope.$apply();
     };
 
     return RoutQueueFactory;
