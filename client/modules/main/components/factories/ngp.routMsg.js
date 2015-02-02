acf6c33e75f4c40c55de9e065212a021c2fe5491
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-02-01 21:57:00 -0500

    AComit a

--- client/modules/main/components/factories/ngp.routMsg.js
+++ client/modules/main/components/factories/ngp.routMsg.js
@@ -4,7 +4,7 @@
 angular.module(ngp.const.app.name)
     .factory('RoutMsg', [
         '$rootScope',
-        //'$scope',$scope,
+        '$scope',
         'Router',
         'Authorization',
         'Chat',
@@ -12,11 +12,11 @@ angular.module(ngp.const.app.name)
         RoutMsg
     ]);
 
-function RoutMsg($rootScope, Router, Authorization, Chat, UtilFunc) {
+function RoutMsg($rootScope,$scope, Router, Authorization, Chat, UtilFunc) {
 
     function RoutMsgFactory(){
         Router.apply(this, arguments);
-        //console.log('$scope', $scope);
+        console.log('$scope', $scope);
     }
 
     RoutMsgFactory.prototype = Object.create(Router.prototype);
