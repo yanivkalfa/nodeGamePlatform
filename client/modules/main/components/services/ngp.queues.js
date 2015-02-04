930663bd5fca5def8b21a79df55d92228d72c79c
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-02-03 19:48:45 -0500

    AComit a

--- client/modules/main/components/services/ngp.queues.js
+++ client/modules/main/components/services/ngp.queues.js
@@ -22,7 +22,7 @@ function Queues(
 
     QueuesService.prototype.add =  function(q){
         var self = this;
-        var args = Array.prototype.slice.call(arguments);
+        var args = Array.prototype.slice.call(arguments, -1,-1);
         args.push(new Queue(q));
         return Lists.prototype.add.apply(self, args);
     };
