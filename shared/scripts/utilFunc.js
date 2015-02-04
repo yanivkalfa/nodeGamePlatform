9ee412ed180838bf7e5f5a327f055212d5bf2f86
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-02-04 16:06:40 -0500

    AComit a

--- shared/scripts/utilFunc.js
+++ shared/scripts/utilFunc.js
@@ -45,7 +45,7 @@ UtilFuncFactory.prototype =  {
 
 if (typeof module !== 'undefined' && module.exports) {
     console.log('aa');
-    module.exports = function(){ return new UtilFuncFactory(); }
+    module.exports = {yaniv : 'a'}
 }
 
 
