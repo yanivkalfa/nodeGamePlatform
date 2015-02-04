e3e62c46d2b66f751ed2ac29b97b6366f2d95edc
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-02-04 17:44:06 -0500

    AComit a

--- client/modules/main/components/factories/ngp.utilFunc.js
+++ client/modules/main/components/factories/ngp.utilFunc.js
@@ -2,34 +2,40 @@
  * Created by Yaniv-Kalfa on 1/2/15.
  */
 
-angular.module(ngp.const.app.name)
-    .factory('UtilFunc', [function () {
+(function(){
+    angular.module(ngp.const.app.name)
+        .factory('UtilFunc', [
+            UtilFunc
+        ]);
+
+    function UtilFunc() {
 
 
-    function UtilFuncFactory(){}
+        function UtilFuncFactory(){}
 
-    UtilFuncFactory.prototype =  {
+        UtilFuncFactory.prototype =  {
 
-        formatMsgDate : function(unixDate) { return moment(new Date(unixDate)).format('HH:mm:ss'); },
+            formatMsgDate : function(unixDate) { return moment(new Date(unixDate)).format('HH:mm:ss'); },
 
-        indexOf : function(thisArg, index, prop){
-            var self = this;
-            return self.findIndex.call(thisArg, index, prop);
-        },
+            indexOf : function(thisArg, index, prop){
+                var self = this;
+                return self.findIndex.call(thisArg, index, prop);
+            },
 
-        findIndex : function(index, prop){
-            for(var i = 0; i < this.length; i++){
-                if(prop) if(this[i][prop] == index || this[i][prop] == index[prop]) return i;
-                if(this[i] == index) return i;
+            findIndex : function(index, prop){
+                for(var i = 0; i < this.length; i++){
+                    if(prop) if(this[i][prop] == index || this[i][prop] == index[prop]) return i;
+                    if(this[i] == index) return i;
+                }
+                return -1;
+            },
+
+            toArray : function(item){
+                if(!item) return false;
+                return !_.isArray(item) ? [item] : item;
             }
-            return -1;
-        },
+        };
 
-        toArray : function(item){
-            if(!item) return false;
-            return !_.isArray(item) ? [item] : item;
-        }
-    };
-
-    return new ngp.oFns.uf();
-}]);
+        return new ngp.oFns.uf();
+    }
+})();
