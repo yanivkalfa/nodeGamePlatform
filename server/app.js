8309a249a4cddaa4dd8894b91ed648b7150d54f2
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-02-04 17:30:00 -0500

    AComit a

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ _s.oReq = require('./lib/requiredFiles.js')(_s); // require files.
 _s.details = JSON.parse(_s.oReq.fs.readFileSync(__dirname + '/serverDetails.json').toString())[process.argv[2] || 'testSlave01'];
 _s.sServerDirname = __dirname; // Server dir
 _s.sClientDirname = _s.oReq.path.resolve(__dirname, '..') + '/client'; //Client dir
+_s.sSharedDirname = _s.oReq.path.resolve(__dirname, '..') + '/shared'; //Client dir
 _s.oConfig = require('./settings/config'); // require config files.
 global.oCore = require('./core')(_s); // require core files.
 _s.oModules = require('./lib/modules')(_s); // require utility functions
