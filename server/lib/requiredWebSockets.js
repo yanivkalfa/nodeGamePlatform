25728c8b0c637fdcb2d52af98c6b5b8e32c7b23f
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-02-05 08:31:15 -0500

    AComit a

--- server/lib/requiredWebSockets.js
+++ server/lib/requiredWebSockets.js
@@ -51,7 +51,7 @@ module.exports = function(_s){
                         console.log(Queues);
                         spark.Queues = new Queues();
 
-                        console.log(spark.Queues);
+                        console.log(spark.Queues.list);
                         var routSocket = new RoutSocket();
 
                         // Update user's spark id in database - in-case its needed
