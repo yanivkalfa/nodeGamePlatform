75f02577ab18175e85ca45518dfd17afb88e6e9c
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-02-04 21:41:27 -0500

    AComit a

--- server/lib/modules/index.js
+++ server/lib/modules/index.js
@@ -15,17 +15,21 @@ module.exports = function(_s){
     rf.Validation = require(pathsList.Validation)();
     rf.ValidationReg = require(pathsList.ValidationReg)(_s,rf);
     rf.ajaxHandler = require(pathsList.ajaxHandler);
+    console.log('got here ');
     rf.RoomHandler = require(pathsList.RoomHandler)(_s,rf);
     rf.GetRooms = require(pathsList.GetRooms)(_s,rf);
     rf.GetRoom = require(pathsList.GetRoom)(_s,rf);
     rf.Router = require(pathsList.Router)();
     rf.RoutMsg = require(pathsList.RoutMsg)(_s,rf);
+    console.log('got here ');
     rf.RoutRemoteMsg = require(pathsList.RoutRemoteMsg)(_s,rf);
     rf.RoutRoom = require(pathsList.RoutRoom)(_s,rf);
     rf.RoutChat = require(pathsList.RoutChat)(_s,rf);
     rf.RoutSjax = require(pathsList.RoutSjax)(_s,rf);
     rf.RoutQueue = require(pathsList.RoutQueue)(_s,rf);
+    console.log('got here ');
     rf.RoutSocket = require(pathsList.RoutSocket)(_s, rf);
+    console.log('got here ');
 
     return rf;
 };
