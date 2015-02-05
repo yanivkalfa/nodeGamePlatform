7046e0a332b4ebf2421f91323156a3c2c6c692a4
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-02-04 22:45:02 -0500

    AComit a

--- server/lib/modules/supers/validation.js
+++ server/lib/modules/supers/validation.js
@@ -1,14 +1,18 @@
-function Validation (details){
-    this._isValid = false;
-    this.validate(details);
-}
 
-Validation.prototype = {
-    validate : function(details){
-        this._isValid = true;
-    },
-    isValid : function(){
-        return this._isValid; }
+module.exports = (function(){
+    function Validation (details){
+        this._isValid = false;
+        this.validate(details);
+    }
 
-};
-module.exports = Validation;
+    Validation.prototype = {
+        validate : function(details){
+            this._isValid = true;
+        },
+        isValid : function(){
+            return this._isValid; }
+
+    };
+
+    return Validation
+});
