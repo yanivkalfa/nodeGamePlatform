6418ca5f7ac2e630c5caf47f247df835153b899b
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-02-04 20:30:55 -0500

    AComit a

--- client/modules/main/components/services/ngp.queues.js
+++ client/modules/main/components/services/ngp.queues.js
@@ -1,45 +1,5 @@
 /**
  * Created by Yaniv-Kalfa on 1/2/15.
  */
-(function(){
-    angular.module(ngp.const.app.name)
-        .service('Queues', [
-            //'List',
-            //'Queue',
-            ngp.oFns.Queues
-        ]);
-
-    function Queues(
-        //List,
-        //Queue
-        ) {
-        /*
-        var List = ngp.oFns;
-        var Queue = ngp.Queue;
-
-        function QueuesService(){
-            List.apply(this,arguments);
-        }
-
-        QueuesService.prototype = Object.create(List.prototype);
-        QueuesService.prototype.constructor = QueuesService;
-
-        QueuesService.prototype.add =  function(q){
-            var self = this;
-            var args = Array.prototype.slice.call(arguments, -1,-1);
-            args.push(new Queue(q));
-            return List.prototype.add.apply(self, args);
-        };
-
-        QueuesService.prototype.remove =  function(id){
-            var self = this;
-            if(_.isEmpty(this.list[id])) return false;
-            this.list[id].end('Queue removed');
-            return delete self.list[id];
-        };
-        */
-
-        console.log('aaa');
-        return new ngp.oFns.Queues();
-    }
-})();
+angular.module(ngp.const.app.name)
+    .service('Queues', [ngp.oFns.Queues]);
