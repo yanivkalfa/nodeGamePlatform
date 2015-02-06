38fe7ca8bead26a85753c5be991a6464e7fbc263
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-02-06 16:38:26 -0500

    AComit a

--- server/lib/modules/routQueue.js
+++ server/lib/modules/routQueue.js
@@ -182,6 +182,7 @@ module.exports = function(_s){
 
                     QueuesApi.add(qDetails).then(function(queue){
                         console.log('queue',queue);
+                        console.log('user',user);
                         user.queues.push(queue);
                         user.save(function (err, user) {
                             if(err) return joinResponse('joinFail','There was an error creating your queue a');
