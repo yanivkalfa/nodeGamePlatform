c3dd4ccd553fd743e1ec7f113cd0d5f28596f943
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-02-05 20:29:01 -0500

    AComit a

--- server/lib/modules/routSocket.js
+++ server/lib/modules/routSocket.js
@@ -2,32 +2,32 @@ module.exports = function(_s){
     var pathsList = _s.oConfig.pathsList
         , router = require(pathsList.Router)
         , RoutChat = require(pathsList.RoutChat)(_s)
+        , routChat = new RoutChat()
         , RoutSjax = require(pathsList.RoutSjax)(_s)
+        , routSjax = new RoutSjax()
         , RoutQueue = require(pathsList.RoutQueue)(_s)
+        , routQueue = new RoutQueue()
         ;
+
+    /*
+    this.RoutChat = new RoutChat();
+    this.RoutSjax = new RoutSjax();
+    this.RoutQueue = new RoutQueue();*/
 
     function RoutSocket (){
         router.apply(this,arguments);
-        this.RoutChat = new RoutChat();
-        this.RoutSjax = new RoutSjax();
-        this.RoutQueue = new RoutQueue();
+
     }
 
     RoutSocket.prototype = Object.create(router.prototype);
     RoutSocket.prototype.constructor = RoutSocket;
 
 
-    RoutSocket.prototype.chat = function(spark, msg){
-        return this.RoutChat.rout.apply(this.RoutChat,arguments);
-    };
+    RoutSocket.prototype.chat = routChat.rout.apply(routChat);
 
-    RoutSocket.prototype.sjax = function(spark, msg){
-        return this.RoutSjax.rout.apply(this.RoutSjax,arguments);
-    };
+    RoutSocket.prototype.sjax = routSjax.rout.apply(routSjax);
 
-    RoutSocket.prototype.queue = function(spark, msg){
-        return this.RoutQueue.apply(this.RoutQueue,arguments);
-    };
+    RoutSocket.prototype.queue = routQueue.apply(routQueue);
 
 
     return RoutSocket;
