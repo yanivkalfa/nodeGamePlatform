1afd8fc84e4b166ccb6af9ef54aae707b7f5f904
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-02-07 11:34:32 -0500

    AComit a

--- client/modules/main/components/controllers/ngp.adminController.js
+++ client/modules/main/components/controllers/ngp.adminController.js
@@ -59,8 +59,7 @@
                 ,qId
                 ;
 
-            console.log('game', g);
-            if(g.isBusy()) return false;
+            if(g.isBusy()) return Notify.error('Your previous action is still in progress');
             g.setBusy(true);
             if(!g.isQueued()) QueueOut.join(queue);
             else {
