aff16686101fc2575a0d7d0eb27ca489a3a71410
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-02-07 11:32:40 -0500

    AComit a

--- client/modules/main/components/factories/ngp.game.js
+++ client/modules/main/components/factories/ngp.game.js
@@ -26,7 +26,7 @@
             },
 
             setBusy : function(state){
-                this._queue = state;
+                this._isBusy = state;
             },
 
             setQueueImage : function(){
@@ -42,7 +42,7 @@
             },
 
             isBusy : function(){
-                return this._queue;
+                return this._isBusy;
             },
 
             isQueued : function(){
