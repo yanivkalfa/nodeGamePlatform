4e3794b32aa8684b85bae9d47c46f1d530242d1c
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-02-14 11:43:19 -0500

    AComit a

--- server/lib/modules/supers/router.js
+++ server/lib/modules/supers/router.js
@@ -4,7 +4,7 @@ Router.prototype =  {
 
     rout: function(spark, msg, a1, a2, a3, a4, a5){
         var self = this;
-        console.log(msg);
+        console.log('rout', msg);
         return self[msg.m](spark, msg.d, a1, a2, a3, a4, a5);
     }
 };
