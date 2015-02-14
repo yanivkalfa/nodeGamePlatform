59c39627efcf4c596b6f82e12578805b764bec17
Yaniv-Kalfa <yanivkalfa@gmail.com>
2015-02-14 12:54:24 -0500

    AComit a

--- server/lib/modules/routQueue.js
+++ server/lib/modules/routQueue.js
@@ -72,7 +72,7 @@ module.exports = function(_s){
             , qName = q.name
             ;
         // getting all un occupied queues that fit a certain name sorting by data and limiting to game amount.
-        QueuesApi.fetchSortLimit({"name" : qName, occupied : false}, 'date', q.userCount).then(function(queues){
+        QueuesApi.fetchSortLimit({"name" : qName, occupied : false}, 'start', q.userCount).then(function(queues){
             console.log('queues : ', queues, 'q.userCount', q.userCount);
             if(_.isArray(queues) && queues.length < q.userCount) return false;
 
